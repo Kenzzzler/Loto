19fab5e23fcf6278100ca9fe3a984fb6a878b83e
kenzler <vkomkov11@gmail.com>
2023-06-06 13:08:17 +0900

    One

--- src/main/kotlin/Host.kt
+++ src/main/kotlin/Host.kt
@@ -3,9 +3,13 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 object Host {
-    suspend fun generateRandomNum(): Flow<Int> = flow {
-        for (i in 1..100) {
+    suspend fun generateRandomNum() = flow {
+//        for (i in 1..100) {
+//            emit((1..90).random())
+//        }
+        while (true) {
             emit((1..90).random())
+            delay(200)
         }
 
     }
--- src/main/kotlin/LotoCard.kt
+++ src/main/kotlin/LotoCard.kt
@@ -1,5 +1,9 @@
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.takeWhile
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class LotoCard {
     val numList : MutableList<MutableList<Int>> = mutableListOf(
@@ -29,8 +33,8 @@ class LotoCard {
             row.forEach { num ->
                 print(if (num == 0)
                         "   ".padEnd(5, ' ')
-                    else if(num == 100)
-                        " X ".padEnd(5, ' ')
+//                    else if(num == 100)
+//                        " X ".padEnd(5, ' ')
                     else
                         " $num ".padEnd(5, ' '))
                 print("║")
@@ -41,26 +45,23 @@ class LotoCard {
 
     }
 
-    suspend fun check() {
-        Host.generateRandomNum().takeWhile {
-            numList[0].all { it == 0 || it == 100 }
-        }
+    suspend fun check() = runBlocking {
+        launch { Host.generateRandomNum()
             .collect { randomNumber: Int ->
-            numList.forEachIndexed { index, ints ->
-                for (i in ints.indices) {
-                    if (ints[i] == randomNumber){
-                        ints[i] = 100
+                numList.forEachIndexed { index, ints ->
+                    for (i in ints.indices) {
+                        if (ints[i] == randomNumber) {
+                            ints[i] = 0
+                        }
                     }
                 }
-            }
-
-
-        }
-
-
-        show()
-
+                show()
+                if (numList[0].all { it == 0 }) {
+                    cancel()
+                }
+            } }
     }
+
 
 
 }
